2c1c9b388c6d664e1934ccb538ccdf6ca127542c
Kevin Valeri <kevinvaleriworks@gmail.com>
2025-01-31 14:19:56 +0700

    Final Update

--- dashboard/polls/models.py
+++ dashboard/polls/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class DimLaptop(models.Model):
+    company = models.CharField(max_length=255)
+    product = models.CharField(max_length=255)
+    typename = models.CharField(max_length=255)
+    inches = models.DecimalField(max_digits=4, decimal_places=1)
+    screen = models.CharField(max_length=255)
+    touchscreen = models.BooleanField()
+    ipspanel = models.BooleanField()
+    retinadisplay = models.BooleanField()
+
+    def __str__(self):
+        return self.product
+
+class FactHarga(models.Model):
+    laptopid = models.IntegerField()
+    spesifikasiid = models.IntegerField()
+    waktuid = models.IntegerField()
+    price_euros = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Laptop ID: {self.laptopid}, Harga: {self.price_euros}"
--- dashboard/polls/views.py
+++ dashboard/polls/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import DimLaptop, FactHarga
 
-# Create your views here.
+def dashboard(request):
+    # Ambil data laptop dan harga
+    laptops = DimLaptop.objects.all()
+    avg_prices = FactHarga.objects.values('laptopid__company').annotate(avg_price=models.Avg('price_euros'))
+
+    context = {
+        'laptops': laptops,
+        'avg_prices': avg_prices,
+    }
+    return render(request, 'dashboard/dashboard.html', context)
